d68a42d499ec2c1599a016bc171177bcf2077988
Aayush Iyer <aayush.iyer@gmail.com>
2012-10-04 18:06:01 -0600

    Blank Rails Template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleTweet::Application.config.secret_token = '2c7d4e4f1321ed14759d49e27b1df5ea819e2de092bf83afdc3b4acee014d0141d42660855fa9f3e0f851bc60c48377efa4d9a9dc93bfc82254b0dda1bf88690'
