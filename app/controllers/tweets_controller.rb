1751cf510f83376b767034829a8d7c86b4d83775
Aayush Iyer <aayush.iyer@gmail.com>
2012-10-06 21:26:54 -0600

    Added mentions, timeline, user timeline

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,5 +9,13 @@ class TweetsController < ApplicationController
 	def timeline
 		@listing = Twitter.home_timeline({:count =>100})
 	end
+
+	def mytweets
+		@mytweets = Twitter.user_timeline({:count =>100})
+	end
+
+	def mentions
+		@mentions = Twitter.mentions_timeline({:count =>100})
+	end
 end
 
