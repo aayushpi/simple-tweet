d14ecc4397bde2d70ed168535a05dcec1fc3a9f3
Aayush Iyer <aayush.iyer@gmail.com>
2012-10-10 11:34:47 -0600

    Any update?

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 #Auth for Twitter. Currently Hardcoded because I have no sense of security.
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "akZeVLUrNqzdDkev9Luo6g", "7t7hd4OMUiMzU3xl6K8Z3TMojJXnpHtJWAP7Sw2a8wM" 
+  provider :twitter, "akZeVLUrNqzdDkev9Luo6g", "7t7hd4OMUiMzU3xl6K8Z3TMojJXnpHtJWAP7Sw2a8wM" #Hide on each github.
 end
 
