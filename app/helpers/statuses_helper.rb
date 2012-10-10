08557fe1920ddb5eb27dcd33d5060dc73ee5f071
Aayush Iyer <aayush.iyer@gmail.com>
2012-10-10 11:02:26 -0600

    @ and # link insertions done

--- app/helpers/statuses_helper.rb
+++ app/helpers/statuses_helper.rb
@@ -1,10 +1,10 @@
 module StatusesHelper
-=begin
+
 	def linkup_mentions_and_hashtags(text)    
 	  text.gsub!(/@([\w]+)(\W)?/, '<a href="http://twitter.com/\1">@\1</a>\2')
 	  text.gsub!(/#([\w]+)(\W)?/, '<a href="http://twitter.com/search?q=%23\1">#\1</a>\2')
-	  text
+	  return text
 	end
-=end
+
 
 end
